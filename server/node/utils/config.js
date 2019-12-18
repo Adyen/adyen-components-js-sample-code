14412f5fbaed1779f8602c3b397c92778066d37b
marcp <marc.pereziribas@adyen.com>
2019-12-18 12:36:25 +0100

    Update Adyen Checkout Components sample server to v51

--- server/node/utils/config.js
+++ server/node/utils/config.js
@@ -1,6 +1,6 @@
 const { CHECKOUT_APIKEY, MERCHANT_ACCOUNT } = process.env;
 
-const API_VERSION = 'v50';
+const API_VERSION = 'v51';
 const CHECKOUT_URL = `https://checkout-test.adyen.com/${API_VERSION}`;
 
 module.exports = {
