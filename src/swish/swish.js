f61a835b0f5890128af15b0bb193297322056ac2
Douglas Greenshields <douglas@usemarkup.com>
2020-02-12 09:40:43 +0000

    add swish component example

---
+++ src/swish/swish.js
@@ -0,0 +1,30 @@
+// 0. Get originKey
+getOriginKey().then(originKey => {
+    // 1. Create an instance of AdyenCheckout
+    const checkout = new AdyenCheckout({
+        originKey, // Mandatory. originKey from Costumer Area
+        environment: 'test',
+        amount: { currency: 'EUR', value: 1000 },
+        onAdditionalDetails: result => {
+            console.log(result);
+        },
+        onError: error => {
+            console.log(error);
+        }
+    });
+
+    const swishData = {
+        paymentMethod: {
+            type: 'swish'
+        },
+        countryCode: 'SE',
+        amount: {
+            currency: 'SEK',
+            value: 10000
+        }
+    };
+
+    makePayment(swishData).then(response => {
+        checkout.createFromAction(response.action).mount('#swish-container');
+    });
+});
