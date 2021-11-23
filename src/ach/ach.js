33086ff1b1b2c45905c24218374449aca9bd59c7
Zac Allen <zac.allen@outlook.com>
2021-11-23 17:56:45 +0100

    updated ach component, optional configuration to be confirmed

--- src/ach/ach.js
+++ src/ach/ach.js
@@ -1,59 +1,72 @@
 // 0. Get clientKey
 getClientKey().then(clientKey => {
-    // 1. Create an instance of AdyenCheckout
-    const checkout = new AdyenCheckout({
-        environment: 'test',
-        clientKey: clientKey // Mandatory. clientKey from Customer Area
-    });
 
-    // 2. Create and mount the Component
-    const ach = checkout
-        .create('ach', {
-            // Optional Configuration
-            // hasHolderName: true,
+    async function initiateComponent() {
+        // 1. Create an instance of AdyenCheckout
+        const checkout = await AdyenCheckout({
+            environment: 'test',
+            clientKey: clientKey // Mandatory. clientKey from Customer Area
+        });
 
-            // Optional. Customize the look and feel of the payment form
-            // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
-            styles: {},
+        // 2. Create and mount the Component
+        const ach = checkout
+            .create('ach', {
+                // Optional Configuration
+                // hasHolderName: false, // Defaults to true
 
-            // Optional. Define custom placeholders for the Ach fields
-            // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
-            placeholders: {
-                // encryptedBankAccountNumber: '9999 9999 9999 9999',
-                // encryptedBankLocationId: '987654321',
-            },
+                // Optional. Customize the look and feel of the payment form
+                // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
+                styles: {
+                    validated: {
+                        color: 'red'
+                      } 
+                },
 
-            // Optionally show a Pay Button
-            showPayButton: true,
+                // Optional. Define custom placeholders for the Ach fields
+                // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
+                placeholders: {
+                    // encryptedBankAccountNumber: '9999 9999 9999 9999',
+                    // encryptedBankLocationId: '987654321',
+                },
 
-            // Events
-            onSubmit: (state, component) => {
-                if (state.isValid) {
-                    // ACH only works in US or PR, with payment in USD
-                    const additionalConfig = {countryCode: state.data.billingAddress.country, amount: { value: 1000, currency: 'USD'}}
-                    makePayment(ach.data, additionalConfig);
-                }
-            },
+                // Optionally show a Pay Button
+                showPayButton: true,
 
-            onChange: (state, component) => {
-                // state.data;
-                // state.isValid;
+                // Events
+                onSubmit: (state, component) => {
+                    if (state.isValid) {
+                        // ACH only works in US or PR, with payment in USD
+                        const additionalConfig = {
+                            countryCode: state.data.billingAddress.country,
+                            amount: {
+                                value: 1000,
+                                currency: 'USD'
+                            }
+                        }
+                        makePayment(ach.data, additionalConfig);
+                    }
+                },
 
-                updateStateContainer(state); // Demo purposes only
-            },
+                onChange: (state, component) => {
+                    // state.data;
+                    // state.isValid;
+                    updateStateContainer(state); // Demo purposes only
+                },
 
-            // Optional: insert address information, if you already have it
-//            data: {
-//                holderName: 'B. Fish',
-//                billingAddress: {
-//                    street: 'Infinite Loop',
-//                        postalCode: '95014',
-//                        city: 'Cupertino',
-//                        houseNumberOrName: '1',
-//                        country: 'US',
-//                        stateOrProvince: 'CA'
-//                }
-//            }
-        })
-        .mount('#ach-container');
+                // Optional: insert address information, if you already have it
+                //            data: {
+                //                holderName: 'B. Fish',
+                //                billingAddress: {
+                //                    street: 'Infinite Loop',
+                //                        postalCode: '95014',
+                //                        city: 'Cupertino',
+                //                        houseNumberOrName: '1',
+                //                        country: 'US',
+                //                        stateOrProvince: 'CA'
+                //                }
+                //            }
+            })
+            .mount('#ach-container');
+    }
+    initiateComponent()
 });
