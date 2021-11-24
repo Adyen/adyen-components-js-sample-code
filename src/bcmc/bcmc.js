83f971109bfb2ba116f321d721620b005601df6c
Zac Allen <zac.allen@outlook.com>
2021-11-24 15:54:29 +0100

    Updated bcmc card

--- src/bcmc/bcmc.js
+++ src/bcmc/bcmc.js
@@ -1,7 +1,9 @@
 // 0. Get origin key
 getClientKey().then(clientKey => {
+    
+    async function initiateComponent() {
     // 1. Create an instance of AdyenCheckout providing the clientKey
-    const checkout = new AdyenCheckout({
+    const checkout = await AdyenCheckout({
         clientKey: clientKey,
         environment: 'test',
         amount: { currency: 'EUR', value: 1000 },
@@ -13,4 +15,6 @@ getClientKey().then(clientKey => {
 
     // 2. Create and mount component.
     checkout.create('bcmc').mount('#bcmc-container');
+    }
+    initiateComponent()
 });
