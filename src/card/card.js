dc7a87d5b0117f62f2e2c88968e0cb2079e6d4a2
Zac Allen <zac.allen@outlook.com>
2021-11-24 16:51:01 +0100

    Updated card component

--- src/card/card.js
+++ src/card/card.js
@@ -1,45 +1,56 @@
 // 0. Get clientKey
 getClientKey().then(clientKey => {
-    // 1. Create an instance of AdyenCheckout
-    const checkout = new AdyenCheckout({
-        environment: 'test',
-        clientKey: clientKey // Mandatory. clientKey from Customer Area
-    });
 
-    // 2. Create and mount the Component
-    const card = checkout
-        .create('card', {
-            // Optional Configuration
-            // hasHolderName: true,
+    async function initiateComponent() {
 
-            // Optional. Customize the look and feel of the payment form
-            // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
-            styles: {},
+        // Optional, define custom placeholders for the Card fields
+        // https://docs.adyen.com/online-payments/web-components/localization-components
+        // const translations = {
+        //     "en-GB": {
+        //         "creditCard.numberField.placeholder": "1234 5678 9012 3456",
+        //         "creditCard.expiryDateField.placeholder": "MM/YY",
+        //     }
+        // };
 
-            // Optional. Define custom placeholders for the Card fields
-            // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
-            placeholders: {
-                // encryptedCardNumber: '9999 9999 9999 9999',
-                // encryptedExpiryDate: '01/22',
-                // encryptedSecurityCode : '123'
-            },
+        // 1. Create an instance of AdyenCheckout
+        const checkout = await AdyenCheckout({
+            environment: 'test',
+            locale: "en-GB",
+            // Optional, define custom placeholders for the Card fields
+            // https://docs.adyen.com/online-payments/web-components/localization-components
+            // translations: translations,
+            clientKey: clientKey // Mandatory. clientKey from Customer Area
+        });
 
-            // Optionally show a Pay Button
-            showPayButton: true,
+        // 2. Create and mount the Component
+        const card = checkout
+            .create('card', {
+                // Optional Configuration
+                // hasHolderName: true,
 
-            // Events
-            onSubmit: (state, component) => {
-                if (state.isValid) {
-                    makePayment(card.data);
+                // Optional. Customize the look and feel of the payment form
+                // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
+                styles: {},
+
+                // Optionally show a Pay Button
+                showPayButton: true,
+
+                // Events
+                onSubmit: (state, component) => {
+                    if (state.isValid) {
+                        makePayment(card.data);
+                    }
+                },
+
+                onChange: (state, component) => {
+                    // state.data;
+                    // state.isValid;
+
+                    updateStateContainer(state); // Demo purposes only
                 }
-            },
+            })
+            .mount('#card-container');
+    }
+    initiateComponent()
 
-            onChange: (state, component) => {
-                // state.data;
-                // state.isValid;
-
-                updateStateContainer(state); // Demo purposes only
-            }
-        })
-        .mount('#card-container');
 });
