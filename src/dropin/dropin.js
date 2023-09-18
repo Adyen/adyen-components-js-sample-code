728cdd472c88079cde69101257b41b9db91ad76e
nicholas <nicholas.spong@adyen.com>
2023-09-18 12:34:12 +0200

    Dropin demo detects what version of adyen.js is being used and instantiates AdyenCheckout accordingly

--- src/dropin/dropin.js
+++ src/dropin/dropin.js
@@ -1,11 +1,42 @@
+/**
+ * IMPORTANT - Set a boolean indicating whether index.html is loading a version of adyen.js (& adyen.css) >= 5.0.0
+ */
+const head = document.head.innerHTML;
+const version = head.substring(head.indexOf('sdk/') + 4, head.indexOf('/adyen'));
+const majorVn = Number(version.substring(0, version.indexOf('.')));
+const IS_VERSION_5 = majorVn >= 5;
+
+const DEFAULT_COUNTRY = 'US';
+
 // 0. Get clientKey
 getClientKey().then(clientKey => {
-    getPaymentMethods().then(async paymentMethodsResponse => {
-        const configuration = {
+    const urlParams = getSearchParameters(window.location.search);
+
+    console.log('### dropin:::: urlParams.countryCode', urlParams.countryCode);
+
+    // Can add request params to this object
+    const pmReqConfig = {countryCode: urlParams.countryCode || DEFAULT_COUNTRY};
+    getPaymentMethods(pmReqConfig).then(async paymentMethodsResponse => {
+
+        paymentMethodsResponse.paymentMethods.reverse();
+
+        // const mountBut = document.getElementById('dropinMount');
+        // mountBut.addEventListener('click', async e => {
+
+        let allowedPMS = urlParams.allowedpms;// e.g. &allowedpms=[scheme,ideal]
+        if(allowedPMS === '[]' || typeof allowedPMS === 'undefined') allowedPMS = [];
+
+        /**
+         * Oldest version of Dropin that will work is 3.0.0 (can pay with card comp). This will require a genuine originKey in the config.
+         * (2.5.0 will work, but this is before the official Dropin release, and trying to pay fails 'cos the card data is not put into a paymentMethods object)
+         */
+        const configObj = {
             environment: 'test',
             clientKey: clientKey, // Mandatory. clientKey from Customer Area
             paymentMethodsResponse,
-            removePaymentMethods: ['paysafecard', 'c_cash'],
+            // removePaymentMethods: ['paysafecard', 'c_cash'],
+            // allowPaymentMethods: ['scheme', 'ideal'],
+            allowPaymentMethods: allowedPMS,
             onChange: state => {
                 updateStateContainer(state); // Demo purposes only
             },
@@ -13,21 +44,45 @@ getClientKey().then(clientKey => {
                 // state.data;
                 // state.isValid;
                 makePayment(state.data);
-            }
+            },
+            paymentMethodsConfiguration: {
+                card: {
+                    brands: ['mc', 'visa', 'amex'],
+                    billingAddressRequired: true,
+                    // data: {
+                    //     billingAddress: {
+                    //         postalCode: '1234AA',
+                    //         country: 'NL'
+                    //     }
+                    // }
+                }
+            },
+            countryCode: 'NL',
         };
 
         // 1. Create an instance of AdyenCheckout
-            const checkout = await AdyenCheckout(configuration);
+        if (!IS_VERSION_5) {
+            window.checkout = new AdyenCheckout(configObj);
+        } else {
+            window.checkout = await AdyenCheckout(configObj);
+        }
 
-            // 2. Create and mount the Component
-            const dropin = checkout
-                .create('dropin', {
-                    // Events
-                    onSelect: activeComponent => {
-                        if (activeComponent.state && activeComponent.state.data) updateStateContainer(activeComponent.data); // Demo purposes only
-                    }
-                })
-                .mount('#dropin-container');
+        // 2. Create and mount the Component
+        window.dropin = checkout
+            .create('dropin', {
+                // Events
+                onSelect: activeComponent => {
+                    if (activeComponent.state && activeComponent.state.data) updateStateContainer(activeComponent.data); // Demo purposes only
+                },
+                showStoredPaymentMethods: false
+            })
+            .mount('#dropin-container');
+
+        // const mountBut = document.getElementById('dropinMount');
+        // mountBut.addEventListener('click', e => {
+            // window.dropin.mount('#dropin-container');
+
+        // });
 
     });
 });
