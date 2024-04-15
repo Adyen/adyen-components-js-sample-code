4f390793267c6965b7ae11dce2ff9fce9a6a8cd9
nicholas <nicholas.spong@adyen.com>
2024-04-15 14:42:53 +0200

    Bcmc comp can work against vn <=5

--- src/bcmc/bcmc.js
+++ src/bcmc/bcmc.js
@@ -1,7 +1,16 @@
+/**
+ * IMPORTANT - Set a boolean indicating whether index.html is loading a version of adyen.js (& adyen.css) >= 5.0.0
+ */
+const head = document.head.innerHTML;
+const version = head.substring(head.indexOf('sdk/') + 4, head.indexOf('/adyen'));
+const majorVn = Number(version.substring(0, version.indexOf('.')));
+const IS_VERSION_5 = majorVn >= 5;
+
 // 0. Get client key
 getClientKey().then(async clientKey => {
+
     // 1. Create an instance of AdyenCheckout providing the clientKey
-    const checkout = await AdyenCheckout({
+    const configObj = {
         clientKey: clientKey,
         environment: 'test',
         amount: {
@@ -12,7 +21,13 @@ getClientKey().then(async clientKey => {
         onSubmit: (state, component) => {
             makePayment(state.data);
         }
-    });
+    }
+
+    if (!IS_VERSION_5) {
+        window.checkout = new AdyenCheckout(configObj);
+    } else {
+        window.checkout = await AdyenCheckout(configObj);
+    }
 
     // 2. Create and mount component.
     checkout.create('bcmc').mount('#bcmc-container');
