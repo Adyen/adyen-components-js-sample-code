f4ca34e35624d39d58eaa67b116045af3e3a6470
nicholas <nicholas.spong@adyen.com>
2024-10-17 13:08:15 +0200

    updating Card to work with v6 if necessary

--- src/card/card.js
+++ src/card/card.js
@@ -29,7 +29,9 @@ handleRedirectResult();
 const head = document.head.innerHTML;
 const version = head.substring(head.indexOf('sdk/') + 4, head.indexOf('/adyen'));
 const majorVn = Number(version.substring(0, version.indexOf('.')));
-const IS_VERSION_5 = majorVn >= 5;
+const IS_VERSION_4_OR_LESS = majorVn < 5;
+const IS_VERSION_5 = majorVn === 5;
+// const IS_VERSION_6 = majorVn >= 5;
 
 // 0. Get clientKey
 Promise.all([ getClientKey(), getPaymentMethods()]).then(async response => {
@@ -50,7 +52,8 @@ Promise.all([ getClientKey(), getPaymentMethods()]).then(async response => {
      */
     const configObj = {
         environment: 'test',
-        locale: "en-GB",
+        locale: "en-US",
+        countryCode: 'NL', // required for v6. Hardcoded in utils.js > paymentsDefaultConfig
         // translations: translations,
         clientKey: response[0],
         paymentMethodsResponse: response[1],
@@ -59,11 +62,11 @@ Promise.all([ getClientKey(), getPaymentMethods()]).then(async response => {
             if (state.isValid) {
 
                 // const config = {};
-                const config = {
-                    additionalData: {allow3DS2: true}, // allows regular, "in app", 3DS2
-                    origin:"http://localhost:3000" // forces v4.3.1 to use the native 3DS Comp (and avoid a "redirect" response)
-                }
-                // const config = {additionalData: {executeThreeD: true}} // forces 3DS2 into MDFlow redirect
+                // const config = {
+                //     additionalData: {allow3DS2: true}, // allows regular, "in app", 3DS2
+                //     origin:"http://localhost:3000" // forces v4.3.1 to use the native 3DS Comp (and avoid a "redirect" response)
+                // }
+                const config = {additionalData: {executeThreeD: true}} // forces 3DS2 into MDFlow redirect
 
                 const cardData = {...card.data}
                 // cardData.browserInfo.screenWidth = 437663051;
@@ -97,11 +100,11 @@ Promise.all([ getClientKey(), getPaymentMethods()]).then(async response => {
         onError: (e)=>{
             console.log('### Checkout config onError:: e=', e);
         },
-        srConfig: {
-            moveFocus: false,
-            showPanel: true,
-            // node: '.sr-panel' // Add SRPanel to another element than the default
-        },
+        // srConfig: {
+        //     moveFocus: false,
+        //     showPanel: true,
+        //     node: '#card-container' // Add SRPanel to another element than the default
+        // },
         // paymentMethodsConfiguration:{
         //     card:{
         //         challengeWindowSize: '05'
@@ -118,11 +121,12 @@ Promise.all([ getClientKey(), getPaymentMethods()]).then(async response => {
         // }
         /**
          * Needed to make adyen.js vn <= 3.10.0 work
-         * For anything < 3.9.0 it has to be a genuine originKey (published for TestCompany against localhost:3000)
+         * For anything < 3.9.0 it has to be a genuine originKey (published for TestCompany against localhost:4000)
          */
         // originKey: 'pub.v2.8115658705713940.aHR0cDovL2xvY2FsaG9zdDozMDAw.cpyWd9ZhlTyrKVQRp34kMJZjIFMcPvP3B5UhgAdhDjc',
+        // originKey: 'pub.v2.8115658705713940.aHR0cDovL2xvY2FsaG9zdDo0MDAw.SQOrkUNaVKPn2XtL7OPDEqhY8u7qWQGJo76CrK33cAg',// localhost:4000
         /**
-         * Needed to mark adyen.js vn <= 2.4.0 work
+         * Needed to make adyen.js vn <= 2.4.0 work
          */
         // loadingContext: 'https://checkoutshopper-test.adyen.com/checkoutshopper/'
         // useOriginalFlow: true
@@ -139,9 +143,9 @@ Promise.all([ getClientKey(), getPaymentMethods()]).then(async response => {
     console.log('### card:::: isUMD=', isUMD);
 
     // 1. Create an instance of AdyenCheckout
-    if (!IS_VERSION_5) {
+    if (IS_VERSION_4_OR_LESS) {
         window.checkout = new AdyenCheckout(configObj);
-    } else {
+    } else if (IS_VERSION_5) {
         window.checkout = await AdyenCheckout(configObj);
 
         // requirejs(["https://checkoutshopper-test.adyen.com/checkoutshopper/sdk/5.28.0/adyen.js"], function(adyenCheckout) {
@@ -151,6 +155,8 @@ Promise.all([ getClientKey(), getPaymentMethods()]).then(async response => {
         //         // window.dropin = checkout.create('dropin', {...
         //     });
         // });
+    }else {
+        window.checkout = await window.AdyenWeb.AdyenCheckout(configObj);
     }
 
     // if (checkout.paymentMethodsResponse.storedPaymentMethods && checkout.paymentMethodsResponse.storedPaymentMethods.length > 0) {
@@ -165,71 +171,78 @@ Promise.all([ getClientKey(), getPaymentMethods()]).then(async response => {
     //         console.log('### card:::: UMD MF!', );
     //     }
     // })
-    
+
+    const cardConfig = {
+        // Optional Configuration
+        // hasHolderName: true,
+        // holderNameRequired: true,
+
+        // Optional. Customize the look and feel of the payment form
+        // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
+        styles: {},
+
+        // brands:['mc', 'bcmc'],
+        brands:['mc', 'visa', 'cup', 'jcp', 'amex'],
+        // brands: ['mc', 'visa', 'amex', 'maestro', 'cup', 'diners', 'discover', 'jcb', 'bijcard'],
+
+        // Optionally show a Pay Button
+        showPayButton: true,
+        enableStoreDetails: true,
+
+        _disableClickToPay: true,
+
+        placeholders: {
+            holderName: 'ph billy bob',
+            encryptedCardNumber: 'ph enter PAN'
+        },
+
+        styles: {
+            base: {
+                // Setting font
+                fontFamily: 'https://fonts.gstatic.com/s/montserrat/v26/JTUQjIg1_i6t8kCHKm459WxRyS7m0dR9pA.woff2'
+            }
+        },
+
+        // challengeWindowSize: '01',
+
+        // billingAddressRequired: true,
+
+
+        // onError: (e)=>{
+        //     console.log('### Card config::onError:: e=', e);
+        // },
+        onBinLookup: (obj)=>{
+            console.log('### Card config::onBinLookup:: obj=', obj);
+        },
+        onFocus: (obj) => {
+            console.log('### Cards::onFocus:: obj',obj);
+        },
+        onBlur: (obj) => {
+            console.log('### Cards::onBlur:: obj',obj);
+        },
+        // configuration: {
+        //     socialSecurityNumberMode: 'show'
+        // }
+        // installmentOptions: {
+        //     mc: {
+        //         values: [1, 2],
+        //         preselectedValue: 2
+        //     },
+        //     visa: {
+        //         values: [1, 2, 3, 4],
+        //         plans: ['regular', 'revolving'],
+        //         preselectedValue: 4
+        //     }
+        // },
+        setStatusAutomatically: false
+    }
+
     // 2. Create and mount the Component
-    window.card = checkout
-        .create('card', {
-            // Optional Configuration
-            // hasHolderName: true,
-            // holderNameRequired: true,
-
-            // Optional. Customize the look and feel of the payment form
-            // https://docs.adyen.com/developers/checkout/api-integration/configure-secured-fields/styling-secured-fields
-            styles: {},
-
-           // brands:['mc', 'visa', 'cartebancaire'],
-           // brands: ['mc', 'visa', 'amex', 'maestro', 'cup', 'diners', 'discover', 'jcb', 'bijcard'],
-
-            // Optionally show a Pay Button
-            showPayButton: true,
-            enableStoreDetails: true,
-
-            _disableClickToPay: true,
-
-            placeholders: {
-                holderName: 'ph billy bob',
-                encryptedCardNumber: 'ph enter PAN'
-            },
-
-            styles: {
-                base: {
-                    // Setting font
-                    fontFamily: 'https://fonts.gstatic.com/s/montserrat/v26/JTUQjIg1_i6t8kCHKm459WxRyS7m0dR9pA.woff2'
-                }
-            },
-
-            // challengeWindowSize: '01',
-
-            // billingAddressRequired: true,
-
-
-            // onError: (e)=>{
-            //     console.log('### Card config::onError:: e=', e);
-            // },
-            onBinLookup: (obj)=>{
-                console.log('### Card config::onBinLookup:: obj=', obj);
-            },
-            onFocus: (obj) => {
-                console.log('### Cards::onFocus:: obj',obj);
-            },
-            onBlur: (obj) => {
-                console.log('### Cards::onBlur:: obj',obj);
-            },
-            // configuration: {
-            //     socialSecurityNumberMode: 'show'
-            // }
-            // installmentOptions: {
-            //     mc: {
-            //         values: [1, 2],
-            //         preselectedValue: 2
-            //     },
-            //     visa: {
-            //         values: [1, 2, 3, 4],
-            //         plans: ['regular', 'revolving'],
-            //         preselectedValue: 4
-            //     }
-            // },
-            setStatusAutomatically: false
-        })
-        .mount('#card-container');
+    if (IS_VERSION_5 || IS_VERSION_4_OR_LESS) {
+        window.card = checkout
+            .create('card', cardConfig)
+            .mount('#card-container');
+    }else{
+        window.card = new window.AdyenWeb.Card(window.checkout, cardConfig).mount('#card-container');
+    }
 });
